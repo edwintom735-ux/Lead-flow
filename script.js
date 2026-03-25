b801bc79ff254fc2e0958017cbb85ff5a1ff3cfe
edwintom735-ux <edwintom735@gmail.com>
2026-03-25 11:52:34 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("generateBtn");
+
+    if (btn) {
+        btn.addEventListener("click", function() {
+            alert("Button works!");
+        });
+    }
+});
